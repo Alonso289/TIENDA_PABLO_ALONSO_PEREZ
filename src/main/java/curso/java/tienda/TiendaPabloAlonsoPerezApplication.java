f5d1a44a6818bdbaeb36e1929061eda935db3f58
Pablo Alonso <alonso11712@gmail.es>
2021-05-06 17:24:41 +0200

    PROYECTO CREADO

---
+++ src/main/java/curso/java/tienda/TiendaPabloAlonsoPerezApplication.java
@@ -0,0 +1,13 @@
+package curso.java.tienda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaPabloAlonsoPerezApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaPabloAlonsoPerezApplication.class, args);
+	}
+
+}
